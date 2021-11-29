cb4e4b7ad84888a7a0c7adc507b57ee48c712ca9
Joe Rollman <joe.rollman@virginpulse.com>
2021-11-28 20:11:43 -0600

    [ANG-5] adds Assign3 and other work done in section 5]

---
+++ angular-project/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,55 @@
+import { Component, ContentChild, ElementRef, Input, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ServerElementComponent implements OnInit {
+  @Input() element: {
+    type: string,
+    name: string,
+    content: string
+  };
+  @Input() name: string;
+  constructor() { 
+    console.log('constructor called!');
+  }
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+    console.log(this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
+}
